e89273f98a692b6d4e9dbe6994951122b5aba1f9
irinadeeva <deeva.irina.a@gmail.com>
2019-12-11 11:44:19 +0300

    03
    some arguments are bigger than an integer => varify_integer(char *p)

--- push_swap.c
+++ push_swap.c
@@ -93,20 +93,46 @@ int		error(void)
 	return (-1);
 }
 
+/* check the data: 
+some arguments aren’t integers,
+some arguments are bigger than an integer => varify_integer(char *p)
+there are duplicates.
+*/
+
+int varify_integer(char *p)
+{
+  int  i;
+
+  i = 0;
+  p[i] == '-' ? i++ : 0;
+  p[i] == '1' || p[i] == '2' ? i++ : exit(error());
+  p[i] == '0' || p[i] == '1' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '4' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '7' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '4' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '8' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '3' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '6' ? i++ : exit(error());
+  p[i] >= '0' && p[i] <= '4' ? i++ : exit(error());
+  if (p[0] == '-')
+    p[i] >= '0' && p[i] <= '8' ? i++ : exit(error());
+  else 
+    p[i] >= '0' && p[i] <= '7' ? i++ : exit(error());
+  return (1);
+}
+
  t_stack    *getlist(char *p)
 {
   t_stack *temporary;
   int i;
 
   i = 0;
-  temporary = (t_stack *)malloc(sizeof(t_stack));
+  (ft_strlen(p) > 11 || (ft_strlen(p) == 11 && p[i] != '-')) ? exit(error()) : 0;
+  (ft_strlen(p) == 10 || (ft_strlen(p) == 11 && p[i] == '-')) ? varify_integer(p) : 0;
+  p[0] == '-' ? i++ : 0;
   while (p[i])
-  {
-    if (ft_isdigit(p[i]) || p[i] == '-')
-        i++;
-    else
-      exit(error());
-  }
+     ft_isdigit(p[i]) ? i++ : exit(error());
+  temporary = (t_stack *)malloc(sizeof(t_stack));
   temporary -> element = ft_atoi(p);
   temporary -> next = NULL;
   return (temporary);
@@ -123,6 +149,7 @@ int   main(int argv, char **argc)
   if (argv == 2)
   {
     p = ft_strsplit(argc[1], ' ');
+    
     while(p[++i]) 
     {
       a = getlist(p[i]);
