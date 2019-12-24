729731b5e1f8c08ae594d2d77a0e4e158524c7af
irinadeeva <deeva.irina.a@gmail.com>
2019-12-24 23:48:33 +0300

    09

--- push_swap.c
+++ push_swap.c
@@ -4,6 +4,17 @@
 t_stack *begin = NULL;
 t_stack *top = NULL;
 
+
+void yellow()
+{
+  printf("\033[1;33m");
+}
+
+void reset () 
+{
+  printf("\033[0m");
+}
+
 void displayStack()
 {
   t_stack *temporary;
@@ -15,7 +26,9 @@ void displayStack()
     {
       temporary = temporary -> next;
       printf("\t%d ", temporary -> element);
+      reset();
       printf("\t%d ", temporary -> for_max_seq);
+      yellow();
     }
  }
 
@@ -77,18 +90,14 @@ int		error(void)
 	return (-1);
 }
 
-int  look_for_max_sequence()
+void  compare_elements(int *max)
 {
-   t_stack  *tmp;
-   t_stack  *tmp_2;
-   int  max;
+  t_stack  *tmp;
+  t_stack  *tmp_2;
 
-  if (begin == top) 
-    return (1);
-   tmp = begin;
-   tmp_2 = tmp->next;
-   max = 1;
-   while (tmp_2)
+  tmp = begin;
+  tmp_2 = tmp->next;
+  while (tmp_2)
    {
      tmp = begin;
      while (tmp->i < tmp_2->i)
@@ -98,15 +107,66 @@ int  look_for_max_sequence()
          if(tmp_2->for_max_seq <= tmp->for_max_seq)
           { 
             tmp_2->for_max_seq = tmp->for_max_seq + 1;
-            max < tmp_2->for_max_seq ? max = tmp_2->for_max_seq : 0;
+            *max < tmp_2->for_max_seq ? *max = tmp_2->for_max_seq : 0;
           }
        }
       tmp = tmp->next;
      }
      tmp_2 = tmp_2->next;
    }
-   return(1);
 }
+
+
+/*void  rra(t_stacks   *s)
+{
+  t_stack  *tmp;
+  t_stack  *tmp2;
+  int i;
+
+  i = 1;
+  tmp = begin;
+  while (s->len_a - i > 1)
+  {
+    tmp = tmp->next;
+    i++;
+  }
+  tmp->next = NULL;
+  tmp2 = top;
+  top = tmp;
+  if (tmp2 != NULL)
+	{
+		tmp2->next = begin;
+		begin = tmp2;
+	}
+}*/
+
+/*void  indexation()
+{
+  t_stack  *tmp;
+  int i;
+  
+  tmp = begin;
+  i = 0;
+  while(tmp->next != top)
+  {
+    tmp->i = i;
+    tmp = tmp-> next;
+    i++;
+  }
+}*/
+
+
+int  look_for_max_sequence()
+{
+  int      max;
+
+  if (begin == top) 
+    return (1);
+  max = 1;
+  compare_elements(&max);
+  return(1);
+}
+
 
 int   main(int argv, char **argc)
 {
